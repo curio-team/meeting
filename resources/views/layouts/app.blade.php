3f9dc3a0317cf267afe1c2c8705863fccb73003b
Bart Roos <bart@studiokaa.co>
2018-04-28 22:48:19 +0200

    Update bootstrap to 4.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <title>Meeting</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     @stack('head')
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
