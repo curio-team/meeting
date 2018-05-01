b767343c921de509cdfc93ad287fff244905b8ac
Bart Roos <bart@studiokaa.co>
2018-05-01 18:11:27 +0200

    Tasks can be stand-alone

--- database/migrations/2018_04_29_165918_create_tasks_table.php
+++ database/migrations/2018_04_29_165918_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('topic_id')->unsigned();
+            $table->integer('topic_id')->unsigned()->nullable();
             $table->string('slug');
             $table->string('owner');
             $table->string('title');
