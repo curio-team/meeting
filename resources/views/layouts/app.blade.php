2aeefa6add73db7811f79242e07d38a519c8f448
Bart Roos <bart@studiokaa.co>
2018-05-03 17:44:32 +0200

    Move footer to bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,31 +13,33 @@
 </head>
 
 <body>
-
-    <nav class="navbar navbar-light bg-info d-print-none">
-        <div class="container">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <a class="navbar-brand" href="{{ route('home') }}">Meeting</a>
-                </li>
-                @yield('more-breadcrumbs')
-            </ol>
-            <div class="btn-group">
-                @yield('buttons-right')
-                <span class="btn btn-outline-light">
-                    <i class="far fa-user"></i> {{ Auth::id() }}
-                </span>
+    
+    <div class="grid-container">
+        <nav class="navbar navbar-light bg-info d-print-none">
+            <div class="container">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a class="navbar-brand" href="{{ route('home') }}">Meeting</a>
+                    </li>
+                    @yield('more-breadcrumbs')
+                </ol>
+                <div class="btn-group">
+                    @yield('buttons-right')
+                    <span class="btn btn-outline-light">
+                        <i class="far fa-user"></i> {{ Auth::id() }}
+                    </span>
+                </div>
             </div>
+        </nav>
+
+        <div class="container main">
+            @yield('content')
         </div>
-    </nav>
 
-    <div class="container main">
-        @yield('content')
+        <footer class="container d-print-none">
+            <p><small>Found a bug? Please create an issue at our <a href="https://github.com/amorocks/meeting/issues" target="_blank">GitHub</a> repository.</small><br /><small>Designed, built and powered by <a target="_blank" href="http://studiokaa.co">studioKaa</a>, for Team ICO at Radius College.</small></p>
+        </footer>
     </div>
-
-    <footer class="container d-print-none">
-        <p><small>Found a bug? Please create an issue at our <a href="https://github.com/amorocks/meeting/issues" target="_blank">GitHub</a> repository.</small><br /><small>Designed, built and powered by <a target="_blank" href="http://studiokaa.co">studioKaa</a>, for Team ICO at Radius College.</small></p>
-    </footer>
 
 </body>
 </html>
